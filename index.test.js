838dea6bfbd595ca9ceda7a1a7d5117ce231238e
Oles Odynets <olchyk89@gmail.com>
2019-01-25 19:02:45 +0100

    toBeNull matcher

--- index.test.js
+++ index.test.js
@@ -7,3 +7,7 @@ test('Test if Add function adds 3 + 2 correctly', () => {
 test('Test if 2 + 2 NOT equal 5', () => {
     expect( functions.add(2, 2) ).not.toBe(5);
 });
+
+test('Test if returns null', () => {
+    expect( functions.returnValue(null) ).toBeNull();
+});
