07981ea59fad765e9e9670eb55d2eb30b9f98f7f
Oles Odynets <olchyk89@gmail.com>
2019-01-25 19:02:03 +0100

    not operator

--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const functions = require('./index');
 test('Test if Add function adds 3 + 2 correctly', () => {
     expect( functions.add(3, 2) ).toBe(5);
 });
+
+test('Test if 2 + 2 NOT equal 5', () => {
+    expect( functions.add(2, 2) ).not.toBe(5);
+});
